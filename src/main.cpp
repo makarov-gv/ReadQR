5ab3c57a278f6d464db5c73ef3d25fb954be4a3a
Georgy Makarov <makarov.prdx@gmail.com>
2023-04-13 16:07:58 +0300

    Reverted to OpenCV

--- src/main.cpp
+++ src/main.cpp
@@ -2,48 +2,27 @@
 #include <algorithm>
 #include <vector>
 #include <zbar.h>
-
 #include <opencv2/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
-using namespace std;
-using namespace cv;
-using namespace zbar;
-
-
 int main()
 {
+  zbar::ImageScanner scanner;
+  scanner.set_config(zbar::ZBAR_QRCODE, zbar::ZBAR_CFG_ENABLE, 1);
 
-// Read image
-Mat im = imread("QRCode.png");
+  cv::Mat im = cv::imread("QRCode.png");
+  cv::Mat imGray;
+  cv::cvtColor(im, imGray, cv::COLOR_BGR2GRAY);
 
-// Find and decode barcodes and QR codes
-  // Create zbar scanner
-ImageScanner scanner;
+  zbar::Image image(im.cols, im.rows, "Y800", (uchar *)imGray.data, im.cols * im.rows);
+  scanner.scan(image);
 
-// Configure scanner
-scanner.set_config(ZBAR_QRCODE, ZBAR_CFG_ENABLE, 1);
-
-// Convert image to grayscale
-Mat imGray;
-cvtColor(im, imGray, COLOR_BGR2GRAY);
-
-// Wrap image data in a zbar image
-Image image(im.cols, im.rows, "Y800", (uchar *)imGray.data, im.cols * im.rows);
-
-// Scan the image for barcodes and QRCodes
-scanner.scan(image);
-
-// Print results
-for(Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol)
-{
-  string data;
-
-  data = symbol->get_data();
-
-  // Print type and data
-  cout << endl << data << endl << endl;
-}
+  for(zbar::Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol)
+  {
+    std::string data;
+    data = symbol->get_data();
+    std::cout << std::endl << data << std::endl << std::endl;
+  }
   return 0;
 }
